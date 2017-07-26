a9cf70530d12bd685fb979a8f913c5f2f42187d3
RaeVaughan <rachelev.tx@gmail.com>
2017-07-25 21:27:41 -0500

    created and populated table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks (task_name, completed) VALUES ("run to target", FALSE);
+INSERT INTO tasks (task_name, completed) VALUES ("do bootcamp homework", FALSE);
+INSERT INTO tasks (task_name, completed) VALUES ("go to bed", FALSE);
