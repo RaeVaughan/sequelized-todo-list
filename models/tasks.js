c5b6582d5aaa51149dcb308ee6060110d8ae3bc2
RaeVaughan <rachelev.tx@gmail.com>
2017-07-25 21:49:03 -0500

    model setup completed

---
+++ models/tasks.js
@@ -0,0 +1,28 @@
+// Import the orm to create functions that will interact with the database
+var orm = require("../config/orm.js");
+
+var task = {
+  all: function(cb) {
+    orm.selectAll("tasks", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.insertOne("tasks", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.updateOne("tasks", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+  // delete: function(condition, cb) {
+  //   orm.delete("cats", condition, function(res) {
+  //     cb(res);
+  //   });
+  // }
+};
+
+// Export the database functions for the controller
+module.exports = task;
